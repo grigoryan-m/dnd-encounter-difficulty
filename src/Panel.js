1968cb7a3338f9532a93de81456430b0ea83a588
grigoryan.m <waldm4rk@gmail.com>
2023-09-19 20:43:10 +0400

    first commit

---
+++ src/Panel.js
@@ -0,0 +1,18 @@
+import "./Panel.css";
+
+function Panel(){
+    return(
+        <div className="Wrapper">
+            <div className="Panel">
+                <section className="Controls">
+
+                </section>
+                <section className="Info">
+
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default Panel;
