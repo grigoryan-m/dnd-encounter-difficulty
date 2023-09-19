2d9b9c6c1bc82a3259036327d1d0fdf7aa71c969
grigoryan.m <waldm4rk@gmail.com>
2023-09-19 22:26:10 +0400

    implementing logic

--- src/Panel.js
+++ src/Panel.js
@@ -1,15 +1,21 @@
 import "./Panel.css";
+import React, {useEffect} from 'react';
 
 function Panel(){
+    useEffect(() => {
+        const monsters = document.getElementById("monsters");
+        const button = document.getElementById("button");
+
+        button.addEventListener("click", ()=>{
+            
+        });
+
+      }, []);
     return(
         <div className="Wrapper">
             <div className="Panel">
-                <section className="Controls">
-
-                </section>
-                <section className="Info">
-
-                </section>
+                <label>Monster characters: <input id="monsters" placeholder="1:1/4, 2:1..."/></label>
+                <button id="button">Calculate!</button>
             </div>
         </div>
     );
